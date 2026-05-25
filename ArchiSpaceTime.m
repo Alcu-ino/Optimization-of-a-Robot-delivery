58c00cdbf2783997e1b9d199d1264ba5d86b8d27
VitoCafagno <v.cafagno@studenti.poliba.it>
2026-05-25 10:42:24 +0200

    Aggiunta struttura per ogni arco con costo energia e tempo

--- ArchiSpaceTime.m
+++ ArchiSpaceTime.m
@@ -1,18 +1,22 @@
 function [A_T] = ArchiSpaceTime(V_T,archi,TAU) %,TAU_T,p_T
 %ArchiSpaceTime(V_T,...) GENERA LA TEN COMPLETA
-A_T={}
-V_T = string(V_T)
+A_T={};
+V_T = string(V_T);
 for i=1:length(archi)
     for t=0:1:47
-        nodoPartenza = extractBetween(archi(i),1,1);
-        nodoArrivo = extractBetween(archi(i),2,2);
+        nodoPartenza = extractBetween(archi(i).nome,1,1);
+        nodoArrivo = extractBetween(archi(i).nome,2,2);
         if ismember(string(string(nodoPartenza)+string(t)), V_T)
             periodo = Periodo(t);
             tarr = t + ceil(TempoPercorrenzaTau(nodoPartenza, nodoArrivo, periodo,TAU));
             %+tempo di servizio
             %energia e peso e costo
             if tarr <=47 && ismember(nodoArrivo+string(tarr),V_T)
-                A_T{end+1} = nodoPartenza+string(t)+nodoArrivo+string(tarr);
+                arco = struct();
+                arco.nome    = nodoPartenza + string(t) + nodoArrivo + string(tarr);
+                arco.costo   = 0;   % <-- inserisci qui la formula del costo
+                arco.energia = 0;   % <-- inserisci qui la formula dell'energia
+                A_T{end+1} = arco;
             end
         end
     end
--- main.m
+++ main.m
@@ -1,7 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %ASSUNZIONI SUL PROBLEMA
 nodi = ['O';'B';'A'];
-archi = ["OA";"OB"];
+archi = [
+    struct('nome',"OA",'costo',0,'energia',0,'tempoPercorrenza',0);
+    struct('nome',"OB",'costo',0,'energia',0,'tempoPercorrenza',0)
+];
 percorrenze = {[0.5 0.8 0.3];[0.9 1 0.3]}; %multipli di 15! evito divisione per dt (ci sta?)
 TAU = dictionary(archi, percorrenze);
 tempi_limite = {[0 12*4];[2*4 12*4];[11*4 12*4]}; %tempi in slot da 15 minuti (orario 8-20)
